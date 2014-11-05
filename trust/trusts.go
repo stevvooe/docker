64a84e5c3eb291e3fbd62226a9c2831a22f8d108
Derek McGowan <derek@mcgstyle.net>
2014-11-05 15:42:54 -0800

    WIP: trust server update

--- trust/trusts.go
+++ trust/trusts.go
@@ -1,8 +1,12 @@
 package trust
 
 import (
+	"bytes"
+	"crypto"
 	"crypto/x509"
+	"encoding/hex"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -21,21 +25,13 @@ type TrustStore struct {
 	caPool        *x509.CertPool
 	graph         trustgraph.TrustGraph
 	expiration    time.Time
-	fetcher       *time.Timer
-	fetchTime     time.Duration
-	autofetch     bool
 	httpClient    *http.Client
-	baseEndpoints map[string]*url.URL
+	graphEndpoint *url.URL
 
 	sync.RWMutex
 }
 
-// defaultFetchtime represents the starting duration to wait between
-// fetching sections of the graph.  Unsuccessful fetches should
-// increase time between fetching.
-const defaultFetchtime = 45 * time.Second
-
-var baseEndpoints = map[string]string{"official": "https://dvjy3tqbc323p.cloudfront.net/trust/official.json"}
+var graphEndpoint = "http://localhost:8048/graph"
 
 func NewTrustStore(path string) (*TrustStore, error) {
 	abspath, err := filepath.Abs(path)
@@ -43,14 +39,9 @@ func NewTrustStore(path string) (*TrustStore, error) {
 		return nil, err
 	}
 
-	// Create base graph url map
-	endpoints := map[string]*url.URL{}
-	for name, endpoint := range baseEndpoints {
-		u, err := url.Parse(endpoint)
-		if err != nil {
-			return nil, err
-		}
-		endpoints[name] = u
+	u, err := url.Parse(graphEndpoint)
+	if err != nil {
+		return nil, err
 	}
 
 	// Load grant files
@@ -58,8 +49,7 @@ func NewTrustStore(path string) (*TrustStore, error) {
 		path:          abspath,
 		caPool:        nil,
 		httpClient:    &http.Client{},
-		fetchTime:     time.Millisecond,
-		baseEndpoints: endpoints,
+		graphEndpoint: u,
 	}
 
 	err = t.reload()
@@ -92,10 +82,6 @@ func (t *TrustStore) reload() error {
 		f.Close()
 	}
 	if len(statements) == 0 {
-		if t.autofetch {
-			log.Debugf("No grants, fetching")
-			t.fetcher = time.AfterFunc(t.fetchTime, t.fetch)
-		}
 		return nil
 	}
 
@@ -108,92 +94,59 @@ func (t *TrustStore) reload() error {
 	t.graph = trustgraph.NewMemoryGraph(grants)
 	log.Debugf("Reloaded graph with %d grants expiring at %s", len(grants), expiration)
 
-	if t.autofetch {
-		nextFetch := expiration.Sub(time.Now())
-		if nextFetch < 0 {
-			nextFetch = defaultFetchtime
-		} else {
-			nextFetch = time.Duration(0.8 * (float64)(nextFetch))
-		}
-		t.fetcher = time.AfterFunc(nextFetch, t.fetch)
-	}
-
 	return nil
 }
 
-func (t *TrustStore) fetchBaseGraph(u *url.URL) (*trustgraph.Statement, error) {
+func (t *TrustStore) getGraph() trustgraph.TrustGraph {
+	t.RLock()
+	defer t.RUnlock()
+	return t.graph
+}
+
+func (t *TrustStore) fetchGraph(signature []byte) error {
+	h := crypto.MD5.New()
+	h.Write(signature)
+	statementFile := path.Join(t.path, fmt.Sprintf("%s.json", hex.EncodeToString(h.Sum(nil))))
+
+	// TODO Check if statementFile already exists and not expired
+
 	req := &http.Request{
-		Method:     "GET",
-		URL:        u,
+		Method:     "POST",
+		URL:        t.graphEndpoint,
 		Proto:      "HTTP/1.1",
 		ProtoMajor: 1,
 		ProtoMinor: 1,
 		Header:     make(http.Header),
-		Body:       nil,
-		Host:       u.Host,
+		Body:       ioutil.NopCloser(bytes.NewReader(signature)),
+		Host:       t.graphEndpoint.Host,
 	}
 
 	resp, err := t.httpClient.Do(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	if resp.StatusCode == 404 {
-		return nil, errors.New("base graph does not exist")
+		return errors.New("base graph does not exist")
 	}
 
 	defer resp.Body.Close()
 
-	return trustgraph.LoadStatement(resp.Body, t.caPool)
-}
-
-// fetch retrieves updated base graphs.  This function cannot error, it
-// should only log errors
-func (t *TrustStore) fetch() {
-	t.Lock()
-	defer t.Unlock()
-
-	if t.autofetch && t.fetcher == nil {
-		// Do nothing ??
-		return
+	statement, err := trustgraph.LoadStatement(resp.Body, t.caPool)
+	if err != nil {
+		return err
 	}
 
-	fetchCount := 0
-	for bg, ep := range t.baseEndpoints {
-		statement, err := t.fetchBaseGraph(ep)
-		if err != nil {
-			log.Infof("Trust graph fetch failed: %s", err)
-			continue
-		}
-		b, err := statement.Bytes()
-		if err != nil {
-			log.Infof("Bad trust graph statement: %s", err)
-			continue
-		}
-		// TODO check if value differs
-		err = ioutil.WriteFile(path.Join(t.path, bg+".json"), b, 0600)
-		if err != nil {
-			log.Infof("Error writing trust graph statement: %s", err)
-		}
-		fetchCount++
+	// Save statement
+	b, err := statement.Bytes()
+	if err != nil {
+		log.Infof("Bad trust graph statement: %s", err)
+		return err
 	}
-	log.Debugf("Fetched %d base graphs at %s", fetchCount, time.Now())
+	err = ioutil.WriteFile(statementFile, b, 0600)
+	if err != nil {
+		log.Infof("Error writing trust graph statement: %s", err)
+		return err
+	}
 
-	if fetchCount > 0 {
-		go func() {
-			err := t.reload()
-			if err != nil {
-				// TODO log
-				log.Infof("Reload of trust graph failed: %s", err)
-			}
-		}()
-		t.fetchTime = defaultFetchtime
-		t.fetcher = nil
-	} else if t.autofetch {
-		maxTime := 10 * defaultFetchtime
-		t.fetchTime = time.Duration(1.5 * (float64)(t.fetchTime+time.Second))
-		if t.fetchTime > maxTime {
-			t.fetchTime = maxTime
-		}
-		t.fetcher = time.AfterFunc(t.fetchTime, t.fetch)
-	}
+	return t.reload()
 }
